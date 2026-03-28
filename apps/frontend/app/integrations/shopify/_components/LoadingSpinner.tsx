80b1b6dd8674920b342409cd53e4493112e838ff
arslan578 <arslanzaheer977@gmail.com>
2026-03-28 14:36:40 +0500

    commercial site removed

--- apps/frontend/app/integrations/shopify/_components/LoadingSpinner.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import { Spinner } from '@shopify/polaris';
-
-export const LoadingSpinner = () => (
-    <div className="flex justify-center p-8">
-        <Spinner size="large" accessibilityLabel="Loading" />
-    </div>
-);
